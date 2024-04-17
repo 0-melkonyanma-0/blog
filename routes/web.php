06d32e28cab24a9a06865b089ef1ff480c27a950
Mark <margar.melkonyan@bk.ru>
2024-04-17 20:22:25 +0300

    cleared web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-});
+/**
+ * Here could be written routes for standalone application
+ */
